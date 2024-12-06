a6932d303bef14623a6788a1809a9f649d90353a
AskatAsh <abdulaskat@gmail.com>
2024-12-06 14:20:38 +0600

    added google font jost with tailwind config

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      
+      <h1 className='font-jost'>Smart Sport</h1>
+      <p className='font-jost'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates labore, soluta at perferendis iusto corrupti! Rerum inventore praesentium accusantium totam nostrum dignissimos culpa, eum est adipisci dolores odio iusto assumenda?</p>
+      
     </>
   )
 }
