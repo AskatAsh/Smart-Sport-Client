08212512b8aed51bee6a24bbadb1d3efa2e152b9
AskatAsh <abdulaskat@gmail.com>
2024-12-07 16:17:52 +0600

    fetch all equipments data and filtered using user email

--- src/pages/MyEquipmentList.jsx
+++ src/pages/MyEquipmentList.jsx
@@ -1,9 +1,23 @@
+import { useContext, useEffect, useState } from "react";
+import { AuthContext } from "../provider/AuthProvider";
 
 const MyEquipmentList = () => {
+    const [myList, setMyList] = useState([]);
+    const {user} = useContext(AuthContext);
+    useEffect(() => {
+        fetch('https://smart-sport-server.vercel.app/allEquipments')
+        .then(res => res.json())
+        .then(data => {
+            const myEquipmentList = data.filter(equipment => equipment?.userEmail == user?.email);
+            setMyList(myEquipmentList);
+        })
+    }, [user])
     return (
-        <div>
-            My equipment list
-        </div>
+        <section className="max-w-7xl w-11/12 mx-auto">
+            <h1>My Equipment List: {myList.length}</h1>
+
+            
+        </section>
     );
 };
 
