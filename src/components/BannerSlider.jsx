8bda514ab8f0beb4425a5c5575bd6719ba81d301
AskatAsh <abdulaskat@gmail.com>
2024-12-08 02:05:37 +0600

    added changes in responsiveness and styles in bannerslider component

--- src/components/BannerSlider.jsx
+++ src/components/BannerSlider.jsx
@@ -8,7 +8,7 @@ import "../../src/slider.css";
 const BannerSlider = () => {
 
   return (
-    <section className="max-w-7xl w-full mx-auto my-10 h-[500px] overflow-hidden border-2 border-black flex items-center justify-center">
+    <section className="max-w-7xl w-11/12 mx-auto my-10 overflow-hidden border-2 border-black">
       <Swiper
         slidesPerView={1}
         spaceBetween={30}
@@ -19,32 +19,32 @@ const BannerSlider = () => {
           clickable: true,
         }}
         navigation={true}
-        autoplay={{
-          delay: 3000,
-          disableOnInteraction: false,
-        }}
+        // autoplay={{
+        //   delay: 3000,
+        //   disableOnInteraction: false,
+        // }}
         modules={[Keyboard, Pagination, Navigation, Autoplay]}
-        className="h-full"
+        className="max-h-[460px] h-full flex overflow-hidden"
       >
         <SwiperSlide>
           <img
             src="https://i.ibb.co/WFRNY1Q/fitness-club.jpg"
             alt="Fitness Club"
-            style={{ width: "100%", height: "auto" }}
+            style={{ width: "auto", height: "100%", aspectRatio: "18/9"}}
           />
         </SwiperSlide>
         <SwiperSlide>
           <img
             src="https://i.ibb.co/BfG7R3J/cycle-equipments.png"
             alt="Cycle Equipment"
-            style={{ width: "100%", height: "auto" }}
+            style={{ width: "auto", height: "100%", aspectRatio: "18/9"}}
           />
         </SwiperSlide>
         <SwiperSlide>
           <img
             src="https://i.ibb.co/6Z6NzXR/fitness-equipment.jpg"
             alt="Fitness Equipment"
-            style={{ width: "100%", height: "auto" }}
+            style={{ width: "auto", height: "100%", aspectRatio: "18/9"}}
           />
         </SwiperSlide>
       </Swiper>
