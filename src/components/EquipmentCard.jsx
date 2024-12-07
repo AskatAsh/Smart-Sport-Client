0a8bcbcf46a865e93e961a4c486656e0fbf04d42
AskatAsh <abdulaskat@gmail.com>
2024-12-07 23:36:29 +0600

    used lottie react animation in equipmentCard for view icon

--- src/components/EquipmentCard.jsx
+++ src/components/EquipmentCard.jsx
@@ -1,50 +1,50 @@
-import { FaEye } from "react-icons/fa6";
 import { RiDeleteBin6Fill, RiEdit2Fill } from "react-icons/ri";
 import { Link } from "react-router-dom";
 import { PropTypes } from "prop-types";
 import Swal from "sweetalert2";
 import { Tooltip as ReactTooltip } from "react-tooltip";
+import viewAnimation from "../assets/viewAnimation.json";
+import Lottie from "lottie-react";
 
 const EquipmentCard = ({ myEquipment, myList, setMyList }) => {
   const { _id, itemName, category, price, rating, image, quantity } =
     myEquipment;
 
-    const handleDelete = (id) => {
-      // console.log(id);
-      Swal.fire({
-          title: "Are you sure?",
-          text: "You won't be able to revert this!",
-          icon: "warning",
-          showCancelButton: true,
-          confirmButtonColor: "#3085d6",
-          cancelButtonColor: "#d33",
-          confirmButtonText: "Yes, delete it!"
-      }).then((result) => {
-          if (result.isConfirmed) {
-              fetch(`https://smart-sport-server.vercel.app/allEquipments/${id}`,{
-                  method: 'DELETE'
-              })
-                  .then(res => res.json())
-                  .then(data => {
-                      // console.log(data);
-                      if (data.deletedCount > 0) {
-                          const remainingEquipments = myList.filter(coffee => coffee._id !== id);
-                          setMyList(remainingEquipments);
-                          Swal.fire({
-                              title: "Deleted!",
-                              text: "This Equipment has been deleted.",
-                              icon: "success"
-                          });
-                      }
-                  })
-
-          }
-      });
-  }
+  const handleDelete = (id) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        fetch(`https://smart-sport-server.vercel.app/allEquipments/${id}`, {
+          method: "DELETE",
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            if (data.deletedCount > 0) {
+              const remainingEquipments = myList.filter(
+                (equipment) => equipment._id !== id
+              );
+              setMyList(remainingEquipments);
+              Swal.fire({
+                title: "Deleted!",
+                text: "This Equipment has been deleted.",
+                icon: "success",
+              });
+            }
+          });
+      }
+    });
+  };
 
   return (
     <div className="text-gray-800 flex flex-col lg:flex-row items-center gap-8 bg-white shadow-md border-2 border-gray-900 p-5 sm:p-8">
-      {/* image */}
+      {/* Image */}
       <div className="lg:h-[240px] flex flex-1 justify-center items-center">
         <img
           className="object-cover w-full h-full"
@@ -52,7 +52,8 @@ const EquipmentCard = ({ myEquipment, myList, setMyList }) => {
           alt={`image of ${itemName}`}
         />
       </div>
-      {/* details */}
+
+      {/* Details */}
       <div className="flex flex-col gap-2 text-xl xl:mr-5 flex-1">
         <p>
           <span className="font-semibold">Name: </span> {itemName}
@@ -70,64 +71,44 @@ const EquipmentCard = ({ myEquipment, myList, setMyList }) => {
           <span className="font-semibold">Price: </span> ${price}
         </p>
       </div>
-      {/* view, update delete */}
+
+      {/* Actions: View, Update, Delete */}
       <div className="flex flex-row lg:flex-col gap-3">
-        <Link
-          to={`/viewDetails/${_id}`}
-          // className="tooltip tooltip-top lg:tooltip-right"
-          // data-tip="View Details"
-          data-tooltip-id="viewDetails"
-        >
-          <button className="bg-gray-200 text-gray-900 w-[40px] h-[40px] flex items-center justify-center text-xl">
-            <FaEye />
-          </button>
+        <Link to={`/viewDetails/${_id}`} data-tooltip-id="viewDetails">
+          <div className="w-[40px] h-[40px] bg-gray-200">
+            <Lottie
+              animationData={viewAnimation}
+              loop={true}
+              style={{ width: 40, height: 40 }}
+            />
+          </div>
         </Link>
-        <Link
-          to={`/updateEquipment/${_id}`}
-          // className="tooltip tooltip-top lg:tooltip-right"
-          // data-tip="Update"
-          data-tooltip-id="update"
-        >
+        <Link to={`/updateEquipment/${_id}`} data-tooltip-id="update">
           <button className="w-[40px] h-[40px] flex items-center justify-center text-xl bg-gray-900 text-white">
             <RiEdit2Fill />
           </button>
         </Link>
-        <Link
-          // className="tooltip tooltip-top lg:tooltip-right"
-          // data-tip="Delete"
+        <button
+          onClick={() => handleDelete(_id)}
           data-tooltip-id="delete"
+          className="w-[40px] h-[40px] flex items-center justify-center text-xl bg-red-600 text-white"
         >
-          <button
-            onClick={() => handleDelete(_id)}
-            className="w-[40px] h-[40px] flex items-center justify-center text-xl bg-red-600 text-white"
-          >
-            <RiDeleteBin6Fill />
-          </button>
-        </Link>
+          <RiDeleteBin6Fill />
+        </button>
       </div>
-      <ReactTooltip
-        id="viewDetails"
-        place="top"
-        content="View"
-      />
-      <ReactTooltip
-        id="update"
-        place="top"
-        content="Update"
-      />
-      <ReactTooltip
-        id="delete"
-        place="top"
-        content="Delete"
-      />
+
+      {/* Tooltips */}
+      <ReactTooltip id="viewDetails" place="top" content="View Details" />
+      <ReactTooltip id="update" place="top" content="Update" />
+      <ReactTooltip id="delete" place="top" content="Delete" />
     </div>
   );
 };
 
 EquipmentCard.propTypes = {
-  myEquipment: PropTypes.object,
-  myList: PropTypes.array,
-  setMyList: PropTypes.func
+  myEquipment: PropTypes.object.isRequired,
+  myList: PropTypes.array.isRequired,
+  setMyList: PropTypes.func.isRequired,
 };
 
 export default EquipmentCard;
