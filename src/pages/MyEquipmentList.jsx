c1e4e7dd93a4f7a28a7418f02ed0f20ad77b47cd
AskatAsh <abdulaskat@gmail.com>
2024-12-07 17:21:21 +0600

    added condition to show equipment list if available

--- src/pages/MyEquipmentList.jsx
+++ src/pages/MyEquipmentList.jsx
@@ -1,6 +1,7 @@
 import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../provider/AuthProvider";
 import EquipmentCard from "../components/EquipmentCard";
+import { Link } from "react-router-dom";
 
 const MyEquipmentList = () => {
   const [myList, setMyList] = useState([]);
@@ -20,11 +21,20 @@ const MyEquipmentList = () => {
       <h1 className="my-8 font-semibold text-3xl text-center">
         My Equipment List
       </h1>
-      <section className="grid grid-cols-1 sm:grid-cols-2 gap-8">
-        {myList.map((myEquipment) => (
-          <EquipmentCard key={myEquipment._id} myEquipment={myEquipment} />
-        ))}
-      </section>
+      {myList.length>0 ? (
+        <>
+          <section className="grid grid-cols-1 sm:grid-cols-2 gap-8">
+            {myList.map((myEquipment) => (
+              <EquipmentCard key={myEquipment._id} myEquipment={myEquipment} />
+            ))}
+          </section>
+        </>
+      ) : (
+        <div className="max-w-3xl mx-auto flex flex-col items-center gap-8">
+            <h2 className="text-5xl text-gray-400 text-center font-bold">You did not add any equipment in the database?</h2>
+            <Link to={'/addEquipment'} className="bg-gray-900 px-10 py-4 text-gray-50 ">Add Equipment</Link>
+        </div>
+      )}
     </section>
   );
 };
