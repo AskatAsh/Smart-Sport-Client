270640838b5d4c03c4a690430d0013ac9275da13
AskatAsh <abdulaskat@gmail.com>
2024-12-08 00:51:48 +0600

    used swiperjs to and customized bannerSlider component

--- src/components/BannerSlider.jsx
+++ src/components/BannerSlider.jsx
@@ -1,10 +1,55 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Keyboard, Navigation, Pagination, Autoplay } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "../../src/slider.css";
 
 const BannerSlider = () => {
-    return (
-        <div>
-            Banner section
-        </div>
-    );
+
+  return (
+    <section className="max-w-7xl w-full mx-auto my-10 h-[500px] overflow-hidden border-2 border-black flex items-center justify-center">
+      <Swiper
+        slidesPerView={1}
+        spaceBetween={30}
+        keyboard={{
+          enabled: true,
+        }}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        autoplay={{
+          delay: 3000,
+          disableOnInteraction: false,
+        }}
+        modules={[Keyboard, Pagination, Navigation, Autoplay]}
+        className="h-full"
+      >
+        <SwiperSlide>
+          <img
+            src="https://i.ibb.co/WFRNY1Q/fitness-club.jpg"
+            alt="Fitness Club"
+            style={{ width: "100%", height: "auto" }}
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            src="https://i.ibb.co/BfG7R3J/cycle-equipments.png"
+            alt="Cycle Equipment"
+            style={{ width: "100%", height: "auto" }}
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            src="https://i.ibb.co/6Z6NzXR/fitness-equipment.jpg"
+            alt="Fitness Equipment"
+            style={{ width: "100%", height: "auto" }}
+          />
+        </SwiperSlide>
+      </Swiper>
+    </section>
+  );
 };
 
 export default BannerSlider;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,10 @@
-import { useContext } from "react";
 import BannerSlider from "../components/BannerSlider";
 import Categories from "../components/Categories";
 import Products from "../components/Products";
-import { AuthContext } from "../provider/AuthProvider";
 
 const Home = () => {
-    const {name} = useContext(AuthContext);
     return (
         <div>
-            <h1>{name} | Home page</h1>
             <BannerSlider />
             <Products />
             <Categories />
