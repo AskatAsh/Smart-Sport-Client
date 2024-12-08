83004b0b5719a46356fd85e03e30960ea6b76cd3
AskatAsh <abdulaskat@gmail.com>
2024-12-08 20:29:20 +0600

    added tailwind class dark: to make the component support darkmode

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,10 +1,12 @@
 import { PropTypes } from "prop-types";
 import { FaArrowRightLong } from "react-icons/fa6";
 import { Link } from "react-router-dom";
+
 const ProductCard = ({ equipment }) => {
   const { _id, itemName, category, price, rating, image, quantity } = equipment;
+
   return (
-    <div className="text-gray-800 flex flex-col lg:flex-row items-center gap-8 bg-white shadow-md border-2 border-gray-900 p-5 sm:p-8">
+    <div className="text-gray-800 dark:text-gray-200 flex flex-col lg:flex-row items-center gap-8 bg-white dark:bg-gray-800 shadow-md border-2 border-gray-900 dark:border-gray-600 p-5 sm:p-8">
       {/* Image */}
       <div className="lg:h-[240px] flex flex-1 justify-center items-center">
         <img
@@ -31,7 +33,12 @@ const ProductCard = ({ equipment }) => {
         <p>
           <span className="font-semibold">Price: </span> ${price}
         </p>
-        <Link to={`/viewDetails/${_id}`} className="text-xl font-semibold text-gray-900 underline underline-offset-2 hover:underline-offset-4 duration-200 flex items-center gap-2 hover:gap-3">View Details <FaArrowRightLong /></Link>
+        <Link
+          to={`/viewDetails/${_id}`}
+          className="text-xl font-semibold text-gray-900 dark:text-gray-100 underline underline-offset-2 hover:underline-offset-4 duration-200 flex items-center gap-2 hover:gap-3"
+        >
+          View Details <FaArrowRightLong />
+        </Link>
       </div>
     </div>
   );
