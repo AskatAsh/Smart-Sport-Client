916b83cc54d27aa834c63da97fafdd8643b0b2e1
AskatAsh <abdulaskat@gmail.com>
2024-12-08 20:40:59 +0600

    initialized firebase and changed categories component to support dark theme

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -12,39 +12,44 @@ const Categories = () => {
       .then((res) => res.json())
       .then((data) => {
         const uniqueCategories = data.reduce((acc, item) => {
-            const exists = acc.find((cat) => cat.category === item.category);
-            if (!exists) {
-              acc.push({ id: item._id, category: item.category });
-            }
-            return acc;
-          }, []);
+          const exists = acc.find((cat) => cat.category === item.category);
+          if (!exists) {
+            acc.push({ id: item._id, category: item.category });
+          }
+          return acc;
+        }, []);
         setCategoryList(uniqueCategories);
       })
       .finally(() => setIsLoading(false));
   }, []);
 
   return (
-    <section className="max-w-7xl w-11/12 mx-auto py-10">
-      <h2 className="my-8 font-semibold text-3xl text-center">
+    <section className="max-w-7xl w-11/12 mx-auto my-16">
+      <h2 className="my-8 font-semibold text-3xl text-center text-gray-800 dark:text-gray-200">
         Explore Our Categories
       </h2>
 
       {isLoading ? (
         <Loading />
       ) : categoryList.length > 0 ? (
-        <ul className="flex flex-wrap justify-center gap-4 sm:gap-6">
+        <ul className="flex flex-wrap justify-center gap-4 sm:gap-6 py-8">
           {categoryList.map((equipment) => (
-            <li className="bg-white border-2 border-gray-300 py-3 px-6 text-lg text-center text-gray-900 font-semibold" key={equipment.id}>{equipment?.category}</li>
+            <li
+              className="bg-white dark:bg-gray-800 border-2 border-gray-300 dark:border-gray-600 py-3 px-6 text-lg text-center text-gray-900 dark:text-gray-100 font-semibold"
+              key={equipment.id}
+            >
+              {equipment?.category}
+            </li>
           ))}
         </ul>
       ) : (
         <div className="max-w-3xl mx-auto flex flex-col items-center gap-8">
-          <h2 className="text-5xl text-gray-400 text-center font-bold">
+          <h2 className="text-5xl text-gray-400 dark:text-gray-600 text-center font-bold">
             You did not add any equipment to the database?
           </h2>
           <Link
             to={"/addEquipment"}
-            className="bg-gray-900 px-10 py-4 text-gray-50"
+            className="bg-gray-900 dark:bg-gray-100 px-10 py-4 text-gray-50 dark:text-gray-900"
           >
             Add Equipment
           </Link>
