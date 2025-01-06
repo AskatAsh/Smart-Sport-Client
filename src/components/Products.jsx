1196ef1f514a711841cb99f3a33a0a4e351b5640
AskatAsh <abdulaskat@gmail.com>
2025-01-06 21:04:01 +0600

    changed grid gap in products component

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -27,7 +27,7 @@ const Products = () => {
       {isLoading ? (
         <Loading />
       ) : equipmentList.length > 0 ? (
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-8">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-5">
           {equipmentList.map((equipment) => (
             <ProductCard
               key={equipment._id}
